7c05e6ae086a750e501055cfe45bec10c43beac6
JungMinyong <wispedia@snu.ac.kr>
2025-03-06 18:04:20 +0900

    fix makefile issues

---
+++ src/enzo/make-happiness15.sh
@@ -0,0 +1,26 @@
+#!/bin/sh  
+
+# This makes enzo.exe on Happiness@SNU
+
+echo "Making enzo on"
+pwd
+make clean
+make default
+cd ../../
+./configure
+cd src/enzo/
+
+#module add icc/latest
+#module add mpi/latest
+export PATH=/home/wispedia/install/mpich-15/bin:$PATH
+
+make machine-linux-mpich15
+make precision-64 integers-32 particle-id-32 max-baryons-30 opt-aggressive lcaperf-no max-tasks-per-node-36 grackle-yes new-problem-types-yes nbody-yes photon-no cuda-no
+make show-config
+make show-flags
+make -j8
+#cp enzo.exe enzo_spare.exe
+#cp enzo.exe /data1/wispedia/enzo_nfw/lessSFB
+echo "Make done!"
+pwd
+date
